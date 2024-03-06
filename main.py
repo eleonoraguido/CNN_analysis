565ecbf6a97604e0dfac6d7bac55c80a2e796fe0
Eleonora Guido <guido@yoakum.hep.physik.uni-siegen.de>
2024-03-06 17:08:55 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <config_file>")
+        sys.exit(1)
+
+    config_file = sys.argv[1]
+    
+
+if __name__ == "__main__":
+    main()
